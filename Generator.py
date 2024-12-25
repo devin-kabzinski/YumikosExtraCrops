6dd36790858faab898321ef2f75356a1c7be0940
SakakiYumiko <007devin@gmail.com>
2024-12-25 12:59:13 -0500

    Small update, starting automation

---
+++ Generator.py
@@ -0,0 +1,7 @@
+cooking_recipies = "data/CookingRecipes.json"
+crops = "data/Crops.json"
+machines = "data/Machines.json"
+objects = "data/Objects.json"
+shop_seeds = "data/ShopSeeds.json"
+
+defaultText = "i18n/default.json"
