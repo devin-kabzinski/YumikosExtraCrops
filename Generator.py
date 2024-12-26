db17b97bfab9b82ebd285b94b053d23af1ffeebe
SakakiYumiko <007devin@gmail.com>
2024-12-25 21:08:57 -0500

    update objects and add fruit tree file

--- Generator.py
+++
@@ -1,7 +0,0 @@
-cooking_recipies = "data/CookingRecipes.json"
-crops = "data/Crops.json"
-machines = "data/Machines.json"
-objects = "data/Objects.json"
-shop_seeds = "data/ShopSeeds.json"
-
-defaultText = "i18n/default.json"
